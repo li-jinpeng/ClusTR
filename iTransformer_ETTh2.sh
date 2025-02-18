6cc1f9864b4c4ac558b2432cfb04bf28bd1c9648
li-jinpeng <3332126450@qq.com>
2025-02-18 22:28:16 +0800

    update readme

--- iTransformer_ETTh2.sh
+++ iTransformer_ETTh2.sh
@@ -2,32 +2,6 @@ export CUDA_VISIBLE_DEVICES=0
 
 model_name=iTransformer
 
-# python -u run.py \
-#   --task_name long_term_forecast \
-#   --is_training 1 \
-#   --root_path ./dataset/ETT-small/ \
-#   --data_path ETTh2.csv \
-#   --model_id ETTh2_96_96 \
-#   --model $model_name \
-#   --data clustr \
-#   --features M \
-#   --seq_len 96 \
-#   --label_len 48 \
-#   --pred_len 96 \
-#   --e_layers 2 \
-#   --d_layers 1 \
-#   --factor 3 \
-#   --enc_in 1 \
-#   --dec_in 1 \
-#   --c_out 1 \
-#   --des 'Exp' \
-#   --d_model 128 \
-#   --d_ff 128 \
-#   --itr 1 \
-#   --cluster_amount 2 \
-#   --cluster_index 0 \
-#   --test_index 1
-
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
